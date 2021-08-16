5b9f20b52d8fe5b96db82cc90ec89141fdb3b401
Soichi Sumi <soichi.sumi@gmail.com>
2021-08-17 06:49:41 +0900

    add glue sample

---
+++ test/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+
+
+@pytest.fixture(scope="session", autouse=True)
+def scope_session():
+    # テスト内で使い回せるようにS3のURLを環境変数に設定
+    os.environ["TEST_S3_ENDPOINT_URL"] = "http://aws.local:4566"
+    sc = SparkContext()
+    # S3のエンドポイントをLocalStackへ差し替える
+    sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "http://aws.local:4566")
+    sc._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+    sc._jsc.hadoopConfiguration().set("fs.s3a.signing-algorithm", "S3SignerType")
+    pytest.sc = sc
+    pytest.glueContext = GlueContext(pytest.sc)
+    pytest.spark = pytest.glueContext.spark_session
